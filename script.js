95bafc85c51de31a244054099771cd08b11fb98d
Nishtha Barnwal <113125393+n273b@users.noreply.github.com>
2024-01-21 19:06:15 +0530

    Added js code for mouseover effect on about section

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const headingEducation = document.querySelector(".h3#education");
+const headingAchievements = document.querySelector(".h3#achieve");
+const headings = document.querySelectorAll(".h3");
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle("active");
@@ -9,4 +12,14 @@ hamburger.addEventListener('click', () => {
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
+}))
+
+document.querySelectorAll(".box-flexbox").forEach(n => n.addEventListener("mouseover", () => {
+    headingEducation.style.color = "#f00090";
+    headingAchievements.style.color = "#f00090";
+}))
+
+document.querySelectorAll(".box-flexbox").forEach(n => n.addEventListener("mouseout", () => {
+    headingEducation.style.color = "#f0f8ff";
+    headingAchievements.style.color = "#f0f8ff";
 }))
