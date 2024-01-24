e12af6fc1efcb3f657545e791b52f52b08088c23
Nishtha Barnwal <113125393+n273b@users.noreply.github.com>
2024-01-24 19:34:43 +0530

    Added the new function for mouseover/out in script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const navMenu = document.querySelector(".nav-menu");
 const headingEducation = document.querySelector(".h3#education");
 const headingAchievements = document.querySelector(".h3#achieve");
 const headings = document.querySelectorAll(".h3");
+const content = document.querySelector(".edu-cont");
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle("active");
@@ -14,12 +15,25 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     navMenu.classList.remove("active");
 }))
 
-document.querySelectorAll(".box-flexbox").forEach(n => n.addEventListener("mouseover", () => {
-    headingEducation.style.color = "#f00090";
-    headingAchievements.style.color = "#f00090";
-}))
+// document.querySelectorAll(".box-flexbox").forEach(n => n.addEventListener("mouseover", () => {
+//     headingEducation.style.color = "#f00090";
+//     headingAchievements.style.color = "#f00090";
+// }))
 
-document.querySelectorAll(".box-flexbox").forEach(n => n.addEventListener("mouseout", () => {
-    headingEducation.style.color = "#f0f8ff";
-    headingAchievements.style.color = "#f0f8ff";
-}))
+// document.querySelectorAll(".box-flexbox").forEach(n => n.addEventListener("mouseout", () => {
+//     headingEducation.style.color = "#f0f8ff";
+//     headingAchievements.style.color = "#f0f8ff";
+// }))
+
+function changeColor(id, isMouseOver) {
+
+    var h3Element = document.getElementById(id);
+
+        if (isMouseOver) {
+            // Change the color on mouseover
+            h3Element.style.color = '#f00090'; // Change to your desired color
+        } else {
+            // Reset the color on mouseout
+            h3Element.style.color = ''; // Reset to default color
+        }
+}
